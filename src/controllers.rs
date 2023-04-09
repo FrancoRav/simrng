4dc8bf1e4c095439d18501bfe386fb12a43ac33d
Franco Raviolo <raviolofranco@gmail.com>
2023-04-08 21:15:13 -0300

    fix: save last generated list for histogram generation

--- src/controllers.rs
+++ src/controllers.rs
@@ -1,9 +1,18 @@
-use axum::{Json, extract};
-use simrng::dist::{exponential, normal_box_muller, normal_convolution, poisson, uniform, UniformData, NormalData, ExponentialData};
+use axum::extract::State;
+use axum::{extract, Json};
+use simrng::dist::{
+    exponential, normal_box_muller, normal_convolution, poisson, uniform, ExponentialData,
+    NormalData, UniformData,
+};
 use simrng::rng::LinearCongruentialGenerator;
-use simrng::stats::{HistogramData, HistogramInput, generate_histogram};
+use simrng::stats::{generate_histogram, HistogramData, HistogramInput};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
-pub async fn get_uniform(data: extract::Json<UniformData>) -> Json<Vec<f64>> {
+pub async fn get_uniform(
+    State(arc): State<Arc<Mutex<Vec<f64>>>>,
+    data: extract::Json<UniformData>,
+) -> Json<Vec<f64>> {
     let seed = data.seed;
     let number = data.number;
     let lower_limit = data.lower;
@@ -13,10 +22,15 @@ pub async fn get_uniform(data: extract::Json<UniformData>) -> Json<Vec<f64>> {
     for _ in 0..number {
         res.push(uniform(&mut rng, lower_limit, upper_limit));
     }
+    let mut arc = arc.lock().await;
+    *arc = res.clone();
     Json(res)
 }
 
-pub async fn get_normal_bm(data: extract::Json<NormalData>) -> Json<Vec<f64>> {
+pub async fn get_normal_bm(
+    State(arc): State<Arc<Mutex<Vec<f64>>>>,
+    data: extract::Json<NormalData>,
+) -> Json<Vec<f64>> {
     let seed = data.seed;
     let number = data.number;
     let mean = data.mean;
@@ -28,10 +42,15 @@ pub async fn get_normal_bm(data: extract::Json<NormalData>) -> Json<Vec<f64>> {
         res.push(rnds.0);
         res.push(rnds.1);
     }
+    let mut arc = arc.lock().await;
+    *arc = res.clone();
     Json(res)
 }
 
-pub async fn get_normal_conv(data: extract::Json<NormalData>) -> Json<Vec<f64>> {
+pub async fn get_normal_conv(
+    State(arc): State<Arc<Mutex<Vec<f64>>>>,
+    data: extract::Json<NormalData>,
+) -> Json<Vec<f64>> {
     let seed = data.seed;
     let number = data.number;
     let mean = data.mean;
@@ -41,10 +60,15 @@ pub async fn get_normal_conv(data: extract::Json<NormalData>) -> Json<Vec<f64>> 
     for _ in 0..number {
         res.push(normal_convolution(&mut rng, mean, sd));
     }
+    let mut arc = arc.lock().await;
+    *arc = res.clone();
     Json(res)
 }
 
-pub async fn get_exponential(data: extract::Json<ExponentialData>) -> Json<Vec<f64>> {
+pub async fn get_exponential(
+    State(arc): State<Arc<Mutex<Vec<f64>>>>,
+    data: extract::Json<ExponentialData>,
+) -> Json<Vec<f64>> {
     let seed = data.seed;
     let number = data.number;
     let lambda = data.lambda;
@@ -53,10 +77,15 @@ pub async fn get_exponential(data: extract::Json<ExponentialData>) -> Json<Vec<f
     for _ in 0..number {
         res.push(exponential(&mut rng, lambda));
     }
+    let mut arc = arc.lock().await;
+    *arc = res.clone();
     Json(res)
 }
 
-pub async fn get_poisson(data: extract::Json<ExponentialData>) -> Json<Vec<f64>> {
+pub async fn get_poisson(
+    State(arc): State<Arc<Mutex<Vec<f64>>>>,
+    data: extract::Json<ExponentialData>,
+) -> Json<Vec<f64>> {
     let seed = data.seed;
     let number = data.number;
     let lambda = data.lambda;
@@ -65,10 +94,16 @@ pub async fn get_poisson(data: extract::Json<ExponentialData>) -> Json<Vec<f64>>
     for _ in 0..number {
         res.push(poisson(&mut rng, lambda));
     }
+    let mut arc = arc.lock().await;
+    *arc = res.clone();
     Json(res)
 }
 
-pub async fn get_histogram(data: extract::Json<HistogramInput>) -> Json<HistogramData> {
+pub async fn get_histogram(
+    State(arc): State<Arc<Mutex<Vec<f64>>>>,
+    data: extract::Json<HistogramInput>,
+) -> Json<HistogramData> {
     let data = data.0;
-    Json(generate_histogram(data))
+    let arc = arc.lock().await;
+    Json(generate_histogram(data, arc))
 }
