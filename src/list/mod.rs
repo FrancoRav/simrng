dc5f24d0da421f66a959747a30a877cf2dc2acbb
Franco Raviolo <raviolofranco@gmail.com>
2023-04-13 00:03:06 -0300

    fix: do not panic if page exceeds maximum, return empty vec

--- src/list/mod.rs
+++ src/list/mod.rs
@@ -4,5 +4,8 @@ pub fn get_page(nums: Arc<Vec<f64>>, pagenum: usize) -> Vec<f64> {
     let page_size = 30;
     let start: usize = page_size * pagenum;
     let end = (start + page_size).min(nums.len());
-    nums.get(start..end).unwrap().to_vec()
+    match nums.get(start..end) {
+        Some(nums) => { nums.to_vec() },
+        None => { vec![] },
+    }
 }
