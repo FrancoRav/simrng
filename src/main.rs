08f64bb456cb06be6f9f4d958dff8543a1706b85
Franco Raviolo <raviolofranco@gmail.com>
2023-04-18 20:08:13 -0300

    fix: log listening port as INFO, not DEBUG

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ async fn main() {
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "api=debug".into()),
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "simrng=info".into()),
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
@@ -43,8 +43,8 @@ async fn main() {
         port = 3000;
     }
     // Crear servidor e iniciar en puerto 3000
-    tracing::debug!("Listening on {}", addr);
     let addr = std::net::SocketAddr::from(([0, 0, 0, 0], port));
+    tracing::info!("Listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
