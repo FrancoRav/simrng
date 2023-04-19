edc353dc4498745df0a4fa4205a0dddecffefc1d
Franco Raviolo <raviolofranco@gmail.com>
2023-04-19 00:57:26 -0300

    fix: page slice correctly calculated

--- src/list/mod.rs
+++ src/list/mod.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 pub fn get_page(nums: Arc<Vec<f64>>, pagenum: usize) -> Vec<f64> {
     let page_size = 30;
-    let start: usize = page_size * pagenum;
+    let start: usize = page_size * (pagenum-1);
     let end = (start + page_size).min(nums.len());
     match nums.get(start..end) {
         Some(nums) => nums.to_vec(),
